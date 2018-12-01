e735e0c05efce9d0e3bb3a123e8de85ae6ef4713
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-12-01 11:46:11 +0800

    :rocket: Enhance find/show by hashed id.
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -8,6 +8,7 @@
 
 namespace App\Repositories;
 
+use App\Criterion\Eloquent\ThisWhereEqualsCriteria;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Prettus\Repository\Contracts\CacheableInterface;
 use Prettus\Repository\Eloquent\BaseRepository as BaseRepo;
@@ -17,13 +18,23 @@ abstract class BaseRepository extends BaseRepo implements CacheableInterface
 {
     use CacheableRepository;
 
+    /**
+     * @param string $key
+     * @param array $columns
+     * @return mixed
+     * @throws \Prettus\Repository\Exceptions\RepositoryException
+     */
     public function firstOrFailedByHashedId(string $key = 'id', $columns = ['*'])
     {
         // https://github.com/laravel/lumen-framework/issues/685#issuecomment-350376018
         // https://github.com/laravel/lumen-framework/issues/685#issuecomment-443393222
         $hashedId = app('request')->route()[2][$key];
 
-        $entity = parent::find(app('hashids')->decode($hashedId), $columns)->first();
+        $id = app('hashids')->decode($hashedId)[0];
+
+        $this->pushCriteria(new ThisWhereEqualsCriteria('id', $id));
+
+        $entity = $this->first($columns);
         if (is_null($entity)) {
             throw  new ModelNotFoundException;
         }
