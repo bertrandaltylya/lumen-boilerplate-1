c8bedc32c0ae96940ee20674d78088474c6b7d7b
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-12-09 19:39:11 +0800

    :microphone: Composer lock update.
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->alias('cache', 'Illuminate\Cache\CacheManager');
+        $this->app->alias('auth', 'Illuminate\Auth\AuthManager');
+
         LumenPassport::routes($this->app->router);
     }
 }
